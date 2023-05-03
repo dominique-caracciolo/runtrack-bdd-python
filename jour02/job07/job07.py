d69fd7e431055614acc5ce5194e65810964ece87
Dominique Caracciolo <dominique.caracciolo@laplateforme.io>
2023-05-03 15:47:05 +0200

    jour2

--- jour02/job07/job07.py
+++ jour02/job07/job07.py
@@ -42,8 +42,59 @@ result = mycursor.fetchall()
 
 
 for x in result:
-   print(x)
+   print("Salaire a plus de 3000 euros:", x)
 
-class Salarie:
-   
+class Employee:
+    columns = ['id', 'nom', 'prenom', 'salaire', 'id_service']
+
+    def __init__(self, host, user, password, database):
+        self.db = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.db.cursor()
+
+    def create_employee_table(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS employes (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nom VARCHAR(255),
+                prenom VARCHAR(255),
+                salaire FLOAT,
+                id_service INT
+            )
+        """)
+        self.db.commit()
+
+    def insert_employee(self, nom, prenom, salaire, id_service):
+        self.cursor.execute("""
+            INSERT INTO employes (nom, prenom, salaire, id_service)
+            VALUES (%s, %s, %s, %s)
+        """, (nom, prenom, salaire, id_service))
+        self.db.commit()
+
+    def get_employee_by_salary(self, seuil_salaire):
+        self.cursor.execute("""
+            SELECT * FROM employes WHERE salaire > %s
+        """, (seuil_salaire,))
+        result = self.cursor.fetchall()
+        return result
+
+    def update_employee_salary(self, id_employee, new_salaire):
+        self.cursor.execute("""
+            UPDATE employes SET salaire = %s WHERE id = %s
+        """, (new_salaire, id_employee))
+        self.db.commit()
+
+    def delete_employee(self, id_employee):
+        self.cursor.execute("""
+            DELETE FROM employes WHERE id = %s
+        """, (id_employee,))
+        self.db.commit()
+
+    def __del__(self):
+        self.db.close()
+
 
